eeb98e8b038f1c6abe4f21c1f05c3f4baca09831
JulioLaz <julioalazarte@gmail.com>
2023-03-01 21:35:01 -0300

    CAMBIO DE CONTRASEÑA TERMINADO

---
+++ src/main/java/com/backend/jal/correo/controller/CorreoController.java
@@ -0,0 +1,87 @@
+package com.backend.jal.correo.controller;
+
+import com.backend.jal.correo.dto.ChangePasswordDTO;
+import com.backend.jal.correo.dto.EmailValuesDTO;
+import com.backend.jal.correo.service.CorreoService;
+import com.backend.jal.security.controller.Mensaje;
+import com.backend.jal.security.entity.Usuario;
+import com.backend.jal.security.service.UsuarioService;
+//import emailpassword.dto.ChangePasswordDTO;
+//import emailpassword.dto.EmailValuesDTO;
+//import emailpassword.service.EmailService;
+import java.util.Optional;
+import java.util.UUID;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/email")
+//@CrossOrigin(origins = {"http://localhost:4200", "https://portfolio-julio-lazarte.web.app"})
+public class CorreoController {
+    
+    @Autowired
+    CorreoService emailService;
+
+    @Autowired
+    UsuarioService usuarioService;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    
+    @Value("${spring.mail.username}")
+    private String mailFrom;
+
+    private static final String subject = "Cambio de Contraseña";
+
+    @PostMapping("enviar")
+    public ResponseEntity<?> sendEmailTemplate(@RequestBody EmailValuesDTO dto) {
+        Optional<Usuario> usuarioOpt = usuarioService.getByNombreUsuarioOrEmail(dto.getMailTo());
+        if(!usuarioOpt.isPresent())
+            return new ResponseEntity(new Mensaje("No existe ningún usuario con esas credenciales"), HttpStatus.NOT_FOUND);
+        Usuario usuario = usuarioOpt.get();
+        dto.setMailFrom(mailFrom);
+        dto.setMailTo(usuario.getEmail());
+        dto.setSubject(subject);
+        dto.setUserName(usuario.getNombreUsuario());
+        UUID uuid = UUID.randomUUID();
+        String tokenPassword = uuid.toString();
+        dto.setTokenPassword(tokenPassword);
+        usuario.setTokenPassword(tokenPassword);
+        usuarioService.save(usuario);
+        emailService.sendEmail(dto);
+        return new ResponseEntity(new Mensaje("Te hemos enviado un correo"), HttpStatus.OK);
+    }
+
+    @GetMapping("email/enviar")
+    public ResponseEntity<?> sendMail() {
+        emailService.sendMail();
+        return new ResponseEntity(new Mensaje("mensaje enviado"), HttpStatus.OK);
+    }    
+    
+    @PostMapping("/change-password")
+    public ResponseEntity<?> changePassword(@Valid @RequestBody ChangePasswordDTO dto, BindingResult bindingResult) {
+        if(bindingResult.hasErrors())
+            return new ResponseEntity(new Mensaje("Campos mal puestos"), HttpStatus.BAD_REQUEST);
+        if(!dto.getPassword().equals(dto.getConfirmPassword()))
+            return new ResponseEntity(new Mensaje("Las contraseñas no coinciden"), HttpStatus.BAD_REQUEST);
+        Optional<Usuario> usuarioOpt = usuarioService.getByTokenPassword(dto.getTokenPassword());
+        if(!usuarioOpt.isPresent())
+            return new ResponseEntity(new Mensaje("No existe ningún usuario con esas credenciales"), HttpStatus.NOT_FOUND);
+        Usuario usuario = usuarioOpt.get();
+        String newPassword = passwordEncoder.encode(dto.getPassword());
+        usuario.setPassword(newPassword);
+        usuario.setTokenPassword(null);
+        usuarioService.save(usuario);
+        return new ResponseEntity(new Mensaje("Contraseña actualizada"), HttpStatus.OK);
+    }    
+}
