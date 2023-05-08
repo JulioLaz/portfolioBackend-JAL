6e48d0a85c791c8542915fdaebb080f773dfb447
JulioLaz <julioalazarte@gmail.com>
2023-05-08 22:22:31 +0100

    permitiendo cors https

---
+++ src/main/java/com/backend/jal/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.backend.jal.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+ 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("https://portfolio-julio-lazarte.web.app", "http://localhost:4200")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(3600);
+    }
+}
+
