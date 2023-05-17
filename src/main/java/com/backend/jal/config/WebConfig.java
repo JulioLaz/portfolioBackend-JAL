61d7f34b91f5e5a1775a0926c9fe7d5a4f658eb9
JulioLaz <julioalazarte@gmail.com>
2023-05-17 22:46:18 +0100

    webConfig

--- src/main/java/com/backend/jal/config/WebConfig.java
+++ src/main/java/com/backend/jal/config/WebConfig.java
@@ -11,11 +11,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("https://portfolio-julio-lazarte.web.app", "http://localhost:4200")
+            .allowedOrigins("https://portfolio-julio-lazarte.web.app/", "http://localhost:4200")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
             .allowedHeaders("*")
             .allowCredentials(true)
             .maxAge(3600);
     }
 }
-
